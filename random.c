89b3882f06f7fa72989cd1907412058a12e9c466
BitBard95 <tuh24888@temple.edu>
2024-09-05 14:46:36 -0400

    Create random.c

---
+++ random.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+
+
+
+// Function to generate a random uppercase letter
+char randchar() {
+    int alpha = rand() % 26;
+    char array[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char rand_char = array[alpha];
+    return rand_char;
+}
